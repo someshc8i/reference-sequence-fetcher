a26fc2c535be6362168ccfe90b2c7eef8a58c8f7
hulksmasher <somesh.08.96@gmail.com>
2018-06-27 15:40:38 +0530

    test_metadata skipped for now

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,8 +1,9 @@
 import json
+import pytest
 
 from click.testing import CliRunner
-from reference_sequence_fetcher.cli import sequence
-# from tests.test_fetcher import get_metadata
+from reference_sequence_fetcher.cli import sequence, metadata
+from tests.test_fetcher import get_metadata
 
 
 def test_sequence(server, data):
@@ -12,10 +13,11 @@ def test_sequence(server, data):
     assert result.exit_code == 0
     assert seq.sequence in result.output
 
-#
-# def test_metadata(server, data):
-#     seq = data[0]
-#     runner = CliRunner()
-#     result = runner.invoke(metadata, [server, seq.md5])
-#     assert result.exit_code == 0
-#     assert get_metadata(seq, seq.sequence) in json.dumps(result.output)
+
+@pytest.mark.skip
+def test_metadata(server, data):
+    seq = data[0]
+    runner = CliRunner()
+    result = runner.invoke(metadata, [server, seq.md5])
+    assert result.exit_code == 0
+    assert get_metadata(seq, seq.sequence) in json.dumps(result.output)
